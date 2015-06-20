4628ea4186e768403b4af3e75c352dc85a409b05
pmckernin <pmckernin@gmail.com>
2015-06-19 22:44:30 -0500

    worked on changed over to use uid in place of ID and then worked on methods to not show previous friend requests

--- app/models/accecpted_friend.rb
+++ app/models/accecpted_friend.rb
@@ -1,5 +1,9 @@
 class AccecptedFriend < ActiveRecord::Base
 
+def user
+User.where("uid == #{self.user_id_1} OR uid ==#{self.user_id_2}")
+end
+
 def at_event
   Attendance.where("user_id == #{self.user_id_1} OR user_id ==#{self.user_id_2}")
 end
--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,11 @@
 class FriendRequest < ActiveRecord::Base
 
+def already_friends
+  AccecptedFriend.where("user_id_1 == #{self.user_id_1} OR user_id_2 ==#{self.user_id_2} AND user_id_1 == #{self.user_id_2} OR user_id_2 ==#{self.user_id_1}")
+end
+
+def user
+User.where("uid == #{self.user_id_1} OR uid ==#{self.user_id_2}")
+end
 
 end
