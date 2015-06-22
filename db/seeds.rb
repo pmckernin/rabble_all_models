479f859a361a7b283ad860cfef7dd061cae25229
pmckernin <pmckernin@gmail.com>
2015-06-22 16:03:56 -0500

    finally wrote seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user_info = [
+  {
+    first: "Brianna",
+    last: "Woods",
+    email: "brianna.woods19@example.com",
+    password: "12341234",
+    password_confirmation: "12341234",
+    age: "23",
+    sex: "female",
+    uid: "12341234"
+  },
+  {
+    first: "Jimmy",
+    last: "Woods",
+    email: "Jwoo@example.com",
+    password: "12341234",
+    password_confirmation: "12341234",
+    age: "23",
+    sex: "male",
+    uid: "12345678"
+  }
+
+]
+
+user_info.each do |user|
+  User.create!(user)
+end
+puts "There are now #{User.count} users in the database."
+
+attendances_seeds = [
+{
+  user_uid: "12341234",
+  event_id: "2"
+  },
+{user_uid: "12345678",
+  event_id: "2"}]
+
+  attendances_seeds.each do |att|
+    Attendace.create!(att)
+  end
+
+  friend_seeds =[
+    {user_id_1: "869449130496",
+      user_id_2: "12341234"
+      },
+       {user_id_1: "869449130496",
+      user_id_2: "12345678"
+      }
+    ]
+
+    attendances_seeds.each do |friendship|
+    AccecptedFriend.create!(friendship)
+  end
+
